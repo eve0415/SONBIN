27a0a1c9b724a6e5eade6bdf0549fe85b3983cdd
eve0415 <40632436+eve0415@users.noreply.github.com>
2024-08-22 10:49:09 +0900

    fix: access token request and add explanatory error

---
+++ backend/oauth/src/error.rs
@@ -0,0 +1,16 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Redis connection lost")]
+    RedisConnectionLost(),
+
+    #[error("State not found: {state}")]
+    InvalidState { state: String },
+
+    #[error("Not a member in the guild")]
+    NotMember,
+
+    #[error("Unknown error: {0}")]
+    Unknown(#[source] anyhow::Error),
+}
+
+pub(crate) type Result<T, E = Error> = std::result::Result<T, E>;
--- backend/oauth/src/lib.rs
+++ backend/oauth/src/lib.rs
@@ -1,22 +1,26 @@
+pub mod error;
+
 use base64::Engine;
-use rand::Rng;
+use rand::random;
 use redis::{AsyncCommands, Client as RedisClient};
-use reqwest::Client as HttpClient;
 use reqwest::ClientBuilder;
+use reqwest::{Client as HttpClient, StatusCode};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
+use std::collections::HashMap;
 use url::Url;
 
 const AUTHORIZATION_URL: &str = "https://discord.com/oauth2/authorize";
 const DISCORD_API_URL: &str = "https://discord.com/api/v10";
 const DISCORD_CDN_URL: &str = "https://cdn.discordapp.com";
 const RESPONSE_TYPE: &str = "code";
-const SCOPE: &str = "identify%20guilds";
+const SCOPE: &str = "identify guilds.members.read";
 const CODE_CHALLENGE_METHOD: &str = "S256";
 const STATE_LIFETIME: u64 = 300;
 const GRANT_TYPE: &str = "authorization_code";
 const GUILD_ID: &str = "1176516474102353950";
 
+#[derive(Clone, Debug)]
 pub struct DiscordOAuth {
     id: String,
     secret: String,
@@ -58,50 +62,90 @@ impl DiscordOAuth {
                 ("client_id", self.id.to_owned()),
                 ("response_type", RESPONSE_TYPE.to_string()),
                 ("scope", SCOPE.to_string()),
-                ("redirect_uri", self.redirect_url.to_owned()),
+                ("redirect_uri", format!("{}/login", self.redirect_url)),
                 ("state", state),
                 ("code_challenge", code_challenge),
                 ("code_challenge_method", CODE_CHALLENGE_METHOD.to_string()),
+                ("prompt", "none".to_string()),
             ],
         )?)
     }
 
-    pub async fn get_user(self, code: String, state: String) -> anyhow::Result<User> {
-        let mut conn = self.redis_client.get_multiplexed_tokio_connection().await?;
+    pub async fn get_user(self, code: String, state: String) -> error::Result<User> {
+        let mut conn = self
+            .redis_client
+            .get_multiplexed_tokio_connection()
+            .await
+            .map_err(|_| error::Error::RedisConnectionLost())?;
         let code_verifier: String = redis::cmd("HGET")
             .arg("oauth")
             .arg(state.to_owned())
             .query_async(&mut conn)
-            .await?;
-        conn.hdel("oauth", state.to_owned()).await?;
+            .await
+            .map_err(|_| error::Error::InvalidState {
+                state: state.to_owned(),
+            })?;
 
-        let form = reqwest::multipart::Form::new()
-            .text("client_id", self.id.to_owned())
-            .text("client_secret", self.secret.to_owned())
-            .text("grant_type", GRANT_TYPE.to_string())
-            .text("code", code)
-            .text("redirect_uri", format!("{}/login", self.redirect_url))
-            .text("code_verifier", code_verifier);
+        conn.hdel("oauth", state.to_owned())
+            .await
+            .map_err(|e| error::Error::Unknown(e.into()))?;
 
-        let res = self
+        let params = HashMap::from([
+            ("client_id", self.id.to_owned()),
+            ("client_secret", self.secret.to_owned()),
+            ("grant_type", GRANT_TYPE.to_string()),
+            ("code", code),
+            ("redirect_uri", format!("{}/login", self.redirect_url)),
+            ("code_verifier", code_verifier),
+        ]);
+
+        let response = self
             .http_client
             .post(format!("{DISCORD_API_URL}/oauth2/token"))
-            .multipart(form)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .form(&params)
             .send()
-            .await?
-            .json::<AccessTokenResponse>()
-            .await?;
+            .await
+            .map_err(|e| {
+                log::error!("{:?}", e);
 
-        let member = self
+                error::Error::Unknown(e.into())
+            })?;
+
+        if response.status() != StatusCode::OK {
+            log::error!("Failed to get access token: {:?}", response.text().await);
+
+            return Err(error::Error::Unknown(anyhow::anyhow!(
+                "Failed to get access token"
+            )));
+        }
+
+        let res = response.json::<AccessTokenResponse>().await.map_err(|e| {
+            log::error!("{:?}", e);
+
+            error::Error::Unknown(e.into())
+        })?;
+
+        let response = self
             .http_client
             .get(format!(
                 "{DISCORD_API_URL}/users/@me/guilds/{GUILD_ID}/member"
             ))
             .bearer_auth(res.access_token)
             .send()
-            .await?
+            .await
+            .map_err(|e| error::Error::Unknown(e.into()))?;
+
+        if response.status() != StatusCode::OK {
+            log::error!("Failed to get guild member: {:?}", response.text().await);
+
+            return Err(error::Error::NotMember);
+        }
+
+        let member = response
             .json::<serenity::model::guild::Member>()
-            .await?;
+            .await
+            .map_err(|e| error::Error::Unknown(e.into()))?;
 
         Ok(User {
             id: member.user.id.to_string(),
@@ -129,18 +173,18 @@ impl DiscordOAuth {
 fn generate_state_and_code_challenge() -> (String, String, String) {
     let state = generate_random_string(16);
     let code_verifier = generate_random_string(32);
-    let code_challenge =
-        base64::engine::general_purpose::URL_SAFE.encode(Sha256::digest(code_verifier.as_bytes()));
+    let code_challenge = base64::engine::general_purpose::URL_SAFE_NO_PAD
+        .encode(Sha256::digest(code_verifier.as_bytes()));
+
+    println!("{}, {}, {}", state, code_verifier, code_challenge);
 
     (state, code_verifier, code_challenge)
 }
 
 fn generate_random_string(size: usize) -> String {
-    rand::thread_rng()
-        .sample_iter(&rand::distributions::Alphanumeric)
-        .take(size)
-        .map(char::from)
-        .collect()
+    let random_bytes: Vec<u8> = (0..size).map(|_| random::<u8>()).collect();
+
+    base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(&random_bytes)
 }
 
 /// Response from Discord's OAuth2 token endpoint
@@ -154,6 +198,7 @@ struct AccessTokenResponse {
     scope: String,
 }
 
+#[derive(Serialize)]
 pub struct User {
     pub id: String,
     pub name: String,
